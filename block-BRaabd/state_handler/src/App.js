41b237cd2aa7ef25fc2d814775f5b40723a32f55
sohailshaik8328 <sohailshaik765@gmail.com>
2021-09-25 17:53:10 +0530

    sdf

---
+++ block-BRaabd/state_handler/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+import "../assets"
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      image : 0
+    }
+  }
+
+  handleBasket = () => {
+    return (
+      this.state.image
+    )
+  }
+  
+  render() {
+    return (
+      <center>
+        <button onClick={this.handleBasket()}>Basketball</button>
+        <button>PubG</button>
+        <button>Tiger</button>
+        <button>Phone</button>
+        <button>Laptop</button>
+        <button>Cricket</button>
+        <img src={} />
+      </center>
+    )
+  }
+}
+
+export default App;

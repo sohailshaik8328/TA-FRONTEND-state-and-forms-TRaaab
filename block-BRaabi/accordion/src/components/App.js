20e7a553f2d0562c8cb5f16a87645a483c4163f7
sohailshaik8328 <sohailshaik765@gmail.com>
2021-10-04 12:03:30 +0530

    adsf

--- block-BRaabi/accordion/src/components/App.js
+++ block-BRaabi/accordion/src/components/App.js
@@ -1,27 +1,7 @@
 import React from "react";
 import "../styles/App.css"
+import data from './data.json'
 
-let questions = [
-  {
-    Q: 'Who is it for?',
-    A:
-      'Recent college graduates, students taking gap year, dropouts, professionals looking to switch careers, anyone learning web development but needs right environment and guidance. No prior programming knowledge required.',
-  },
-  {
-    Q: 'Does AltCampus help me get a job?',
-    A:
-      'Yes. All we can. We help you at every stage, help you build strong portfolio, prepare for interviews, put you in touch with companies for interview.',
-  },
-  {
-    Q: 'How does the entire process work?',
-    A: 'You can read about it here - How it works.',
-  },
-  {
-    Q: 'Is this course self-paced or batched?',
-    A:
-      'This program follows a blended approach. Students can move at their own pace, however they will be matched with peers learning same concepts.',
-  },
-];
 
 
 
@@ -29,26 +9,30 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      question : ""
+    activeIndex : null
     }
-  }
-
-  handleClick = () => {
-    this.setState({
-      question : questions.map(question => <p>{question.A}</p>)
-    })
   }
 
   render() {
     return (
       <>
-      <center>
-        <h1>Accordian</h1>
-        {
-          questions.map(question => <button onClick={this.handleClick} className="question">{question.Q}</button>)
-        }
-      </center>
-    </>
+        <center>
+          <ul>
+            {
+              data.map((question, active) => {
+                return (
+                  <li>
+                    <h2 onClick={() => {
+                      this.setState({activeIndex : this.state.activeIndex === active ? null : active})
+                    }} className="question">{question.Q}</h2>
+                   {active === this.state.activeIndex &&  <p className="answer">{question.A}</p>}
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </center>
+      </>
     );
   }
 }
